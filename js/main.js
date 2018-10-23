7e287c99cae4efad03f7aa3834ff8e5a96ad00c3
Andrew Nessin <andrewnessinjim@yahoo.com>
2018-10-24 01:52:48 +0530

    fix: Relative path for service worker script

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ document.body.addEventListener('keydown', function() {
   document.body.classList.remove('using-mouse');
 });
 
-navigator.serviceWorker.register('/sw.js', {scope: '/'});
+navigator.serviceWorker.register('./sw.js', {scope: '/'});
 
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
